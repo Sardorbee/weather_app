28c7ccd8482075c0d7a26d8718c8e4b1bac2825d
Sardorbee <sardorgoyibnazarov@gmail.com>
2023-07-17 09:55:17 +0500

    added 3d icons

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,5 @@
   const String apiKey = '42c104c7ce8095343bad605dae6524b3'; // Replace with your actual API key
+  const String GmapsapiKey = 'AIzaSyB1wnLEWH4Iqf2mwLW2qYYMPgC2c7O-f-A'; // Replace with your actual API key
 DateTime parseUnixTimestamp(int unixTimestamp) {
   return DateTime.fromMillisecondsSinceEpoch(unixTimestamp * 1000);
 }
